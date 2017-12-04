008c3eebdc2d92ec9d0baf8d9e1fa3271e34c9c1
tianwen0003 <32695781+tianwen0003@users.noreply.github.com>
2017-12-04 21:28:28 +0800

    Add files via upload

---
+++ scrips/run_fastqc_all.sh
@@ -0,0 +1,4 @@
+for sample in control drought_1h heat_1h heat+drought_6h
+do
+		fastqc -o /home/jiajinbu/nas3/lzz/10_16_rna-seq/fastqc/fastqc_result -f fastq -t 12 --nogroup /home/jiajinbu/nas3/lzz/10_16_rna-seq/row_data/conver_dir/${sample}*.gz
+done
